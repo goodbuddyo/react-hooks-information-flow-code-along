dc698a25e76fb73fd1521bc636968f6dcead8763
Seano <sean01234@yahoo.com>
2023-06-18 15:51:57 -0400

    minor updates

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,23 @@
 import React from "react";
-import { getRandomColor } from "./randomColorGenerator.js";
+import {getRandomColor} from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child({onChangeColor,color}) {
+  //return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+  function handleClick() {
+    const newColor=getRandomColor();
+    onChangeColor(newColor);
+  }
+
+  console.log(onChangeColor);
+  return (
+    <div
+      //onClick={onChangeColor}
+      onClick={handleClick}
+      className="child"
+      style={{backgroundColor: color}}
+    //style={{backgroundColor: "#FFF"}}
+    />
+  );
 }
 
 export default Child;
